1e34870f6a486d45ba198389ffffa4054295bf40
Megan <megan.miller@smoldev.dev>
2020-06-05 02:36:45 -0500

    Index component created, implemented local/naive delete functionality.

---
+++ src/components/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="index container">
+    <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
+      <div class="card-content">
+        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
+        <h2 class="indigo-text">{{smoothie.title}}</h2>
+        <ul class="ingredients">
+          <li v-for="(ing, index) in smoothie.ingredients" :key="index">
+            <span class="chip">{{ing}}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      smoothies: [
+        {
+          id: "1",
+          title: "Chocolate Delight",
+          slug: "chocolate-delight",
+          ingredients: ["chocolate", "strawberries", "cherries"]
+        },
+        {
+          id: "2",
+          title: "Chocolate Banana",
+          slug: "chocolate-banana",
+          ingredients: ["chocolate", "Bananas"]
+        }
+      ]
+    };
+  },
+  methods: {
+    deleteSmoothie(id) {
+      this.smoothies = this.smoothies.filter(smoothie => smoothie.id !== id);
+    }
+  }
+};
+</script>
+
+<style>
+.index {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin: 60px;
+}
+.index h2 {
+  font-size: 1.8em;
+  text-align: center;
+  margin-top: 0;
+}
+.index .ingredients {
+  margin: 30px auto;
+}
+.index .ingredients li {
+  display: inline-block;
+}
+.index .delete {
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  cursor: pointer;
+  color: #aaa;
+  font-size: 1.4em;
+}
+</style>
