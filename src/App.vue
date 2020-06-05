54c3766b4d80b8e3056b17b6d6c26671f1cb936d
Megan <megan.miller@smoldev.dev>
2020-06-05 01:07:45 -0500

    Navbar added.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <Navbar />
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
 export default {
   name: "App",
-  components: {}
+  components: {
+    Navbar
+  }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="navbar">
+    <nav class="nav-extended indigo bg-darken-2">
+      <div class="nav-content">
+        <router-link to="/">
+          <span class="nav-title">Smoothies</span>
+        </router-link>
+        <a class="btn-floating btn-large halfway-fab pink">
+          <router-link to="/add">
+            <i class="material-icons">add</i>
+          </router-link>
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+.navbar nav {
+  padding: 0 20px;
+}
+</style>
